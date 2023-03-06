9f907a15dd15373d66c4fd716b3a4ee17d65ab78
paulubi93 <poolubi93@gmail.com>
2023-03-06 23:01:02 +0100

    modificacion: resta y demo

--- calculadora.py
+++ calculadora.py
@@ -5,5 +5,11 @@ def suma(numero1, numero2):
     total=numero1 + numero2
     return total 
 
-resultado = suma(num1, num2)
-print('el resultado es: ',resultado)
+def resta(numero1, numero2):
+    total=numero1 - numero2
+    return total 
+
+resultado1 = suma(num1, num2)
+resultado2 = resta(num1, num2)
+print('el resultado de suma es: ',resultado1)
+print('el resultado de resta es: ',resultado2)
