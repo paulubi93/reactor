5c074145e3430da7c25b861bdbc0df8dc1db1f76
poolubi93 <poolubi93@gmail.com>
2023-03-18 18:02:14 +0100

    Power BI docs

---
+++ Demo Git y GitHub/calculadora.py
@@ -0,0 +1,15 @@
+num1 = 4
+num2= -3
+ 
+def suma(numero1, numero2):
+    total=numero1 + numero2
+    return total 
+
+def resta(numero1, numero2):
+    total=numero1 - numero2
+    return total 
+
+resultado1 = suma(num1, num2)
+resultado2 = resta(num1, num2)
+print('el resultado de suma es: ',resultado1)
+print('el resultado de resta es: ',resultado2)
